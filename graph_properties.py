d967e9ef26a2a48977905bff6d3680de211751f9
mikl123 <bulesnmisha3@gmil.com>
2022-12-21 18:31:58 +0200

    Main

---
+++ graph_properties.py
@@ -0,0 +1,36 @@
+"""
+Team 12 project
+"""
+from typing import Dict,List
+import os
+import copy
+from collections import defaultdict
+import argparse
+
+def read_csv(path:str, orientated:bool=False) -> Dict[int, List[int]]:
+    pass
+
+def write_csv(graph:dict, dst:str, orientated:bool=False)-> None:
+    pass
+
+def bfs(graph,**kwargs)->list:
+    pass
+
+def find_connectivity_components(graph:dict)->List[int]:
+    pass
+
+def connecting_points(graph:dict)->list:
+    pass
+
+def find_bridges(graph: dict) -> list:
+    pass
+
+def strong_connectivity_components(graph : dict, all_scc = None) ->List[int] :
+    pass
+
+def main():
+    pass
+if __name__=="__main__":
+    main()
+    import doctest
+    doctest.testmod()
